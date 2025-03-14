ad6f4318ff8d7deee7b6b74682f5d0073f058d5c
pierreluc.robitaille <pierreluc.robitaille@rmds.ca>
2025-03-14 14:02:15 -0400

    added pre installed mason plugins

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "autopep8",
+      "codelldb",
+      "html-lsp",
+      "lua-language-server",
+      "prettierd",
+      "pyright",
+      "roslyn",
+      "rust-analyzer",
+      "rzls",
+      "shfmt",
+      "stylua",
+    },
+  },
+}
