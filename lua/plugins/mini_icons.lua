ee35a23df8a83895814539b2d54344c458989020
pierreluc.robitaille <pierreluc.robitaille@rmds.ca>
2025-07-30 14:13:27 -0400

    Adding colors and icons

---
+++ lua/plugins/mini_icons.lua
@@ -0,0 +1,15 @@
+return {
+  "echasnovski/mini.icons",
+  opts = {
+    filetype = {
+      cmake = { glyph = "" },
+      ld = { glyph = "" },
+    },
+    directory = {
+      [".vscode"] = { glyph = "" },
+    },
+    -- extension - {
+    --   h = { H },
+    -- },
+  },
+}
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,5 +1,20 @@
+-- require("nvim-web-devicons").setup({
+--   default = true,
+--   override = {
+--     c = {
+--       icon = "",
+--       name = "C",
+--     },
+--   },
+-- })
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
   opts = {
     filesystem = {
       filtered_items = {
@@ -8,6 +23,16 @@ return {
         always_show = {
           ".gitignore",
           ".cargo",
+          ".vscode",
+        },
+      },
+    },
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          modified = "󰙏",
+          unstaged = "󰄰",
+          staged = "󰄴",
         },
       },
     },
