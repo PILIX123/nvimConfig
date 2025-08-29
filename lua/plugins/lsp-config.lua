183b32b4f98e1227656dec0cbd1397122e0da739
pierreluc.robitaille <pierreluc.robitaille@rmds.ca>
2025-08-29 15:17:42 -0400

    Fixing <leader>ss to add variables

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,6 +8,26 @@ return {
           "--background-index",
           "--query-driver=c:\\PROGRA~2\\ARMGNU~1\\14EFD8~1.2RE\\bin\\AR19DD~1.EXE",
         },
+        keys = {
+          {
+            "<leader>ch",
+            function()
+              local params = { uri = vim.uri_from_bufnr(0) }
+              vim.lsp.buf_request(0, "textDocument/switchSourceHeader", params, function(err, result)
+                if err then
+                  vim.notify("Error switching to source/header" .. err.message, vim.log.levels.ERROR)
+                  return
+                end
+                if not result or result == "" then
+                  vim.notify("No corresponding file found", vim.log.levels.WARN)
+                  return
+                end
+                vim.cmd("edit " .. vim.uri_to_fname(result))
+              end)
+            end,
+            desc = "Switch to source/header",
+          },
+        },
       },
       rust_analyzer = {
         settings = {
